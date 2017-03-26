548359bf2bda035d328b65a681b5e6ae2e04ced7
mylanhui <472303924@qq.com>
2017-03-26 21:26:21 +0800

    Revert "1111111111"
    This reverts commit c3a0d6ccb70b2a2d8817a230893f238b71d2e26e.
    
    回滚

--- ys/admina/urls.py
+++ ys/admina/urls.py
@@ -4,9 +4,12 @@ from django.conf.urls import url
 from admina import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^login/$', views.login),
+    url(r'^$', views.login),
+    url(r'^index/$', views.index),
     url(r'^add_news/$', views.add_news),
-    url(r'^img/$', views.img),
-    url(r'^img_form/$', views.img_form),
+    url(r'^check_news/$', views.check_news),
+    url(r'^add_admin/$', views.add_admin),
+    url(r'^check_admin/$', views.check_admin),
+    url(r'^create_admin/$', views.create_admin),
+    url(r'^page1$',views.col_page1),
 ]
